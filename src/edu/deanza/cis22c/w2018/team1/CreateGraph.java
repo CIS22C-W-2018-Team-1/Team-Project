643d6074c3b9b77e4079c8060385e1b1291b0f0f
roei-burstein <burstein.roei@gmail.com>
2018-03-15 20:28:59 -0700

    Created Skeleton of CreateGraph.java and created an example GraphObject.json

--- src/edu/deanza/cis22c/w2018/team1/CreateGraph.java
+++ src/edu/deanza/cis22c/w2018/team1/CreateGraph.java
@@ -1,5 +1,15 @@
 package edu.deanza.cis22c.w2018.team1;
 
 public class CreateGraph {
-
+	public staic void main(String[] args) {
+		Gson gson = new Gson();
+	}
+	
+	private static voidSerializeGraph() {
+		
+	}
+	
+	private static void deserializeGraph() {
+		
+	}
 }
