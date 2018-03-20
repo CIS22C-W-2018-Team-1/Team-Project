52c6911de2da6a8035aa0ba2eb455cb26359ca6f
roei-burstein <37027610+roei-burstein@users.noreply.github.com>
2018-03-19 19:05:18 -0700

    Update CreateGraph.java

--- src/edu/deanza/cis22c/w2018/team1/CreateGraph.java
+++ src/edu/deanza/cis22c/w2018/team1/CreateGraph.java
@@ -1,17 +1,28 @@
-package edu.deanza.cis22c.w2018.team1;
+import com.google.gson.Gson;
 
 public class CreateGraph {
-	public staic void main(String[] args) {
-	}
-	
-	private static void serializeGraph() {
-		Graph graph = new Graph();
-		
-		Gson gson = new Gson();
-		String json = gson.toJson(graph);
-	}
-	
-	private static void deserializeGraph() {
-		
-	}
+
+    public static void main(String args[]) {
+        serializeGraph();
+        deserializeGraph();
+    }
+
+    private static void serializeGraph() {
+        Graph<Vertex<String>> graph = new Graph<Vertex<String>>();
+        Vertex<String> a = new Vertex<String>("a");
+        Vertex<String> b = new Vertex<String>("b");
+
+        graph.addEdge(a, b, 3);
+        graph.addEdge(b, a, 8);
+
+        Gson g = new Gson();
+        String json = g.toJson(graph);
+    }
+
+    private static void deserializeGraph() {
+        String userJson = "";
+
+        Gson g = new Gson();
+        Graph graph = g.fromJson(userJson, Graph.class);
+    }
 }
