8cbecfb35c388ff3c91d49b0478257ed25dd8f7d
Dimitriye Danilovic <dimitriye@danilovic.net>
2018-03-19 19:44:42 -0700

    Fix CreateGraph

--- src/edu/deanza/cis22c/w2018/team1/CreateGraph.java
+++ src/edu/deanza/cis22c/w2018/team1/CreateGraph.java
@@ -1,28 +1,28 @@
+package edu.deanza.cis22c.w2018.team1;
+
 import com.google.gson.Gson;
 
 public class CreateGraph {
 
-    public static void main(String args[]) {
-        serializeGraph();
-        deserializeGraph();
-    }
+	public static void main(String args[]) {
+		serializeGraph();
+		deserializeGraph();
+	}
 
-    private static void serializeGraph() {
-        Graph<Vertex<String>> graph = new Graph<Vertex<String>>();
-        Vertex<String> a = new Vertex<String>("a");
-        Vertex<String> b = new Vertex<String>("b");
+	private static void serializeGraph() {
+		Graph<String> graph = new Graph<String>();
 
-        graph.addEdge(a, b, 3);
-        graph.addEdge(b, a, 8);
+		graph.addEdge("a", "b", 3);
+		graph.addEdge("b", "a", 8);
 
-        Gson g = new Gson();
-        String json = g.toJson(graph);
-    }
+		Gson g = new Gson();
+		String json = g.toJson(graph);
+	}
 
-    private static void deserializeGraph() {
-        String userJson = "";
+	private static void deserializeGraph() {
+		String userJson = "";
 
-        Gson g = new Gson();
-        Graph graph = g.fromJson(userJson, Graph.class);
-    }
+		Gson g = new Gson();
+		Graph graph = g.fromJson(userJson, Graph.class);
+	}
 }
